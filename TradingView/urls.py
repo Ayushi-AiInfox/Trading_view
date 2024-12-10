32c5691788e0d4097fe63fba5c3e687b04f25c68
Ayushi-AiInfox <ayushiraj@aiinfox.com>
2024-12-10 19:08:06 +0530

    Your commit message

--- TradingView/urls.py
+++ TradingView/urls.py
@@ -7,23 +7,18 @@ from django.contrib.auth.decorators import login_required
 # View for the root path to redirect to the login page
 def root_redirect(request):
     if request.user.is_authenticated:
-        return HttpResponseRedirect('accounts/login')    # If logged in, go to the home page (dashboard)
+        return HttpResponseRedirect('accounts/login')    
     
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', root_redirect, name='root'),  # Redirect root to login if not logged in
-    path('accounts/', include('accounts.urls')),  # Include accounts app URLs (login, signup, etc.)
+    path('admin/', include('admin.urls')),
+    path('', root_redirect, name='root'),  
+    path('accounts/', include('accounts.urls')),  
 ]
 
 
 
-
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-]
+# urlpatterns = [
+#     path('admin/', include(('admin.urls', 'admin'), namespace='admin')),
+#     path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
+# ]
--- admin/models.py
+++ admin/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-class AdminProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # Add any additional fields you need
-    # Example: role, permissions, etc.
+from datetime import datetime
+ 
+class User(AbstractUser):
+    email=models.EmailField(unique=True)
+    verified_at = models.CharField(max_length=200,default='False')
+    role =models.CharField(max_length=200,default='user')
+    status = models.CharField(max_length=20, default='1')
+    updated_at = models.CharField(max_length=200,default=datetime.utcnow())
+    created_at = models.CharField(max_length=200,default=datetime.utcnow())
+    remember_token=models.CharField(max_length=200,default='False')
+    phone_no=models.CharField(max_length=200,null=True)
+    activation_date=models.CharField(max_length=200,default='N/A')
+    class Meta:
+        db_table='users'
+        
